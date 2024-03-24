b299b38070809f9118c43568deed5de746ef6d3b
Am1d1v <dimadiman1adima@gmail.com>
2024-03-24 18:00:59 +0300

    Class Getter/Setter Example

--- ClassesE1/GettersSetters.js
+++ ClassesE1/GettersSetters.js
@@ -1,13 +1,42 @@
 
 
+// Object Getter/Setter Exaple
 
-const task = {
+/* const task = {
     title: 'Task1',
-    dueTo: new Date('2025/01/01'),
+    dueTo: new Date('2024/01/01'),
 
     get isOverdue(){
         return this.dueTo < new Date()
+    },
+
+    set isOverdue(isTaskOverdue){
+        // If task is overdued input new date
+        if(isTaskOverdue){
+            this.dueTo = new Date('2026/01/01')
+        }
     }
 }
 
 console.log(task.isOverdue);
+
+task.isOverdue = true
+console.log(task); */
+
+
+
+// Class Getter/Setter Example
+class Task{
+    constructor(title, dueDate){
+        this.title = title;
+        this.dueDate = dueDate;
+    }
+
+    get isOverdue(){
+        return this.dueDate < new Date();
+    }
+
+}   
+
+const newTask = new Task('Task2', new Date('2024/01/01'))
+
